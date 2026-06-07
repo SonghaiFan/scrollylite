7603415f9edcc41dc0719527b07bdd54f70e42d6
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2026-06-08 07:06:25 +1000

    Update manifest.js

--- src/charts/manifest.js
+++ src/charts/manifest.js
@@ -1,6 +1,6 @@
 // Generated from src/charts/*/index.js.
 // Run scripts/sync-chart-manifest.mjs after adding or removing a chart idiom folder.
-import * as bar from "./bar/index.js?v=semantic-key-1";
+import * as bar from "./bar/index.js?v=semantic-key-2";
 import * as line from "./line/index.js?v=semantic-key-1";
 import * as scatter from "./scatter/index.js?v=semantic-key-1";
 import * as unit from "./unit/index.js?v=semantic-key-1";
